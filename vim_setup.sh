687ac33a7967dca1d4c7d101ce11d0b960123df4
ConradKay <93210131+ConradKay@users.noreply.github.com>
2023-11-28 15:32:01 +0100

    Update vim_setup.sh
    Add some basic vim configuration

--- vim_setup.sh
+++ vim_setup.sh
@@ -39,6 +39,61 @@ call plug#end()
 " Configure vim-racer
 let g:racer_cmd = '$HOME/.cargo/bin/racer'
 let g:racer_cmd_timeout = 5000
+
+" Enable line numbers
+set number
+
+" Enable syntax highlighting
+syntax enable
+
+" Highlight search results as you type
+set incsearch
+
+" Highlight matching parentheses
+set showmatch
+
+" Enable mouse support (if your terminal supports it)
+set mouse=a
+
+" Enable clipboard support (requires Vim to be compiled with +clipboard)
+set clipboard=unnamedplus
+
+" Enable auto-indentation
+filetype plugin indent on
+set smartindent
+set autoindent
+set tabstop=4
+set shiftwidth=4
+set expandtab
+
+" Enable line wrapping
+set wrap
+
+" Configure colorscheme (change 'desert' to your preferred colorscheme)
+colorscheme desert
+
+" Enable airline status bar
+let g:airline#extensions#tabline#enabled = 1
+let g:airline#extensions#tabline#left_sep = ''
+let g:airline#extensions#tabline#right_sep = ''
+let g:airline#extensions#tabline#left_alt_sep = ''
+let g:airline#extensions#tabline#right_alt_sep = ''
+
+" Configure Fugitive for Git integration
+au FileType git setlocal noexpandtab
+
+" Set leader key to comma
+let mapleader = ","
+
+" Map leader+w to save and leader+q to quit
+nnoremap <leader>w :w<CR>
+nnoremap <leader>q :q<CR>
+
+" Map leader+s to save and quit
+nnoremap <leader>s :wq<CR>
+
+" Map leader+c to close a buffer
+nnoremap <leader>c :bd<CR>
 EOT
 
 echo "Vim plugins configured."
